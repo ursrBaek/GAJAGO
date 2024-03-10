10dcf23ac79c76093add4a84b0d200719f942f78
ursrBaek <ursrbaek@gmail.com>
2024-03-10 22:44:10 +0900

    feat: 지역 표시 스티커 컴포넌트 분리

---
+++ src/components/MyTrips/PostIt.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyledSticker } from './styles';
+
+function PostIt({ region }) {
+  const regionObj = {
+    Seoul: '서울특별시',
+    Busan: '부산광역시',
+    DaeGu: '대구광역시',
+    InCheon: '인천광역시',
+    DaeJeon: '대전광역시',
+    GwangJu: '광주광역시',
+    UlSan: '울산광역시',
+    SeJong: '세종자치시',
+    GyeongGi: '경기도',
+    GangWon: '강원도',
+    ChungBuk: '충청북도',
+    ChungNam: '충청남도',
+    JeonBuk: '전라북도',
+    JeonNam: '전라남도',
+    GyeongBuk: '경상북도',
+    GyeongNam: '경상남도',
+    JeJu: '제주도',
+    overseas: '해외',
+  };
+  return <StyledSticker className="region">{regionObj[region]}</StyledSticker>;
+}
+
+export default PostIt;
--- src/components/MyTrips/PrevTripMemo.jsx
+++ src/components/MyTrips/PrevTripMemo.jsx
@@ -1,27 +1,8 @@
 import React from 'react';
+import PostIt from './PostIt';
 import { NoPrevPlan, PlanMemo, PrevPlan } from './styles';
 
 function PrevTripMemo({ prevTrip, howManyDaysAgo }) {
-  const regionObj = {
-    Seoul: '서울특별시',
-    Busan: '부산광역시',
-    DaeGu: '대구광역시',
-    InCheon: '인천광역시',
-    DaeJeon: '대전광역시',
-    GwangJu: '광주광역시',
-    UlSan: '울산광역시',
-    SeJong: '세종시',
-    GyeongGi: '경기도',
-    GangWon: '강원도',
-    ChungBuk: '충청북도',
-    ChungNam: '충청남도',
-    JeonBuk: '전라북도',
-    JeonNam: '전라남도',
-    GyeongBuk: '경상북도',
-    GyeongNam: '경상남도',
-    JeJu: '제주도',
-    overseas: '해외',
-  };
   return (
     <PlanMemo className="prev">
       {prevTrip ? (
@@ -31,7 +12,7 @@ function PrevTripMemo({ prevTrip, howManyDaysAgo }) {
             <p>
               <span className="days">{howManyDaysAgo}</span>일 전
             </p>
-            <div className="region">{regionObj[prevTrip.region]}</div>
+            <PostIt region={prevTrip.region} />
           </div>
           <div className="detailAddrMsg">
             '<span className="detailAddr">{prevTrip.detailAddress}</span>
