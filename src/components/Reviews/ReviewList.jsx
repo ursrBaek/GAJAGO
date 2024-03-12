6adedf5fdd78bfd0a1d6476ffe23169ca5b40755
ursrBaek <ursrbaek@gmail.com>
2024-03-12 10:50:59 +0900

    style: react scroll bar 라이브러리 추가 및 ui 적용

--- src/components/Reviews/ReviewList.jsx
+++ src/components/Reviews/ReviewList.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 import ReviewOfTrip from './ReviewOfTrip';
 import { generateWrittenReviewArray, generateTripsObjectByRegion } from './utils';
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 function ReviewList({ selectedRegion }) {
   const planArray = useSelector((state) => state.user.planData);
@@ -20,16 +21,18 @@ function ReviewList({ selectedRegion }) {
 
   return (
     <ul>
-      {tripsObjectByRegion[selectedRegion].length ? (
-        tripsObjectByRegion[selectedRegion].map((trip) => {
-          const id = trip.title + trip.startDate;
-          return (
-            <ReviewOfTrip key={id} id={id} trip={trip} selected={id === clickedReview} handleClick={handleClick} />
-          );
-        })
-      ) : (
-        <p className="noWrittenReviewMsg">작성된 여행 후기가 없습니다.</p>
-      )}
+      <Scrollbars autoHide>
+        {tripsObjectByRegion[selectedRegion].length ? (
+          tripsObjectByRegion[selectedRegion].map((trip) => {
+            const id = trip.title + trip.startDate;
+            return (
+              <ReviewOfTrip key={id} id={id} trip={trip} selected={id === clickedReview} handleClick={handleClick} />
+            );
+          })
+        ) : (
+          <p className="noWrittenReviewMsg">작성된 여행 후기가 없습니다.</p>
+        )}
+      </Scrollbars>
     </ul>
   );
 }
